735cc783429719f754813c1d00a4904132ff7a8c
Samin Sarker <saminsarker05@gmail.com>
2024-01-17 14:20:35 -0500

    working in virtual envi

--- google-clone/src/App.js
+++ google-clone/src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
+      <h1>Hello World</h1>
+      <Home/>
       
     </div>
+
+    
+
+    // Home page
+    // search page - results page
   );
 }
 
---
+++ google-clone/src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Home.css'
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className='home-header'>
+        <div className='home-header-left'>
+
+        </div>
+        <div className='home-header-right'>
+
+        </div>
+
+      </div>
+      <div className='home-body'></div>
+      
+
+    </div>
+  )
+}
+
+export default Home;
