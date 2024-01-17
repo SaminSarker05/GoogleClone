9615dde75b4c08dfa1c534f348de2abfd5d17f7b
Samin Sarker <saminsarker05@gmail.com>
2024-01-17 14:39:36 -0500

    adding routing between pages

--- google-clone/src/App.js
+++ google-clone/src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <Home/>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/search" element={<div>Search Page</div>}/>
+
+      </Routes>
+
+      {/* <Router>
+        <Switch>
+          <Route path='/search'>
+            <h1>Hello World</h1>
+            <Home/>
+          </Route>
+
+        </Switch>
+        
+
+      </Router> */}
       
+
     </div>
 
     
--- google-clone/src/pages/Home.js
+++ google-clone/src/pages/Home.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import './Home.css'
+import './Home.css';
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className='home'>
       <div className='home-header'>
         <div className='home-header-left'>
+          <Link to="about">about</Link>
 
         </div>
         <div className='home-header-right'>
+          hello world
 
         </div>
 
