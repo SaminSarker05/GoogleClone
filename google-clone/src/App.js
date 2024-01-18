5443cdbf7502fbe44cb6e466d889f2421287533f
Samin Sarker <saminsarker05@gmail.com>
2024-01-17 22:07:02 -0500

    starting search page

--- google-clone/src/App.js
+++ google-clone/src/App.js
@@ -2,20 +2,16 @@ import React from 'react';
 import './App.css';
 import Home from './pages/Home';
 import { Routes, Route } from "react-router-dom";
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
     <div className="App">
-
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/search" element={<div>Search Page</div>}/>
+        <Route path="/search" element={<SearchPage/>}/>
       </Routes>     
-
     </div>
-
-    // Home page
-    // search page - results page
   );
 }
 
