145b7b98ac40e47d36933a38fc4f98063f4f1392
Samin Sarker <saminsarker05@gmail.com>
2024-01-17 21:54:02 -0500

    adding reducer and stateprovidedr

--- google-clone/src/index.js
+++ google-clone/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { StateProvider } from './StateProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+      </StateProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
