f102e8fe86582df65d1446d91c2ad8723371377b
Samin Sarker <saminsarker05@gmail.com>
2024-01-19 00:46:05 -0500

    toolbar finished

--- google-clone/src/pages/SearchPage.js
+++ google-clone/src/pages/SearchPage.js
@@ -5,6 +5,12 @@ import './SearchPage.css';
 import response from '../Response';
 import { Link } from '@mui/material';
 import Search from '../components/SearchBar';
+import SearchIcon from '@mui/icons-material/Search';
+import PhotoIcon from '@mui/icons-material/Photo';
+import SellIcon from '@mui/icons-material/Sell';
+import RoomIcon from '@mui/icons-material/Room';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import FeedIcon from '@mui/icons-material/Feed';
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
@@ -24,6 +30,44 @@ function SearchPage() {
         </Link>
         <div className='search-page-header-body'>
           <Search hideButtons />
+          <div className='search-page-options'>
+            <div className='search-page-options-left'>
+              <div className='search-page-option'>
+                <SearchIcon />
+                <Link to='/all'>All</Link>
+              </div>
+              <div className='search-page-option'>
+                <FeedIcon />
+                <Link to='/news'>News</Link>
+              </div>
+              <div className='search-page-option'>
+                <PhotoIcon />
+                <Link to='/all'>Images</Link>
+              </div>
+              <div className='search-page-option'>
+                <SellIcon />
+                <Link to='/all'>Shopping</Link>
+              </div>
+              <div className='search-page-option'>
+                <RoomIcon />
+                <Link to='/all'>Maps</Link>
+              </div>
+              <div className='search-page-option'>
+                <MoreVertIcon />
+                <Link to='/all'>More</Link>
+              </div>
+            </div>
+            <div className='search-page-options-right'>
+            <div className='search-page-option'>
+                <Link to='/all'>Settings</Link>
+              </div>
+              <div className='search-page-option'>
+                <Link to='/all'>Tools</Link>
+              </div>
+
+            </div>
+
+          </div>
 
         </div>
         
