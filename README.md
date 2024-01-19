45167374da94f2a81c9a31f8b2621abfa5c1d44a
Samin Sarker <saminsarker05@gmail.com>
2024-01-19 00:26:54 -0500

    building search page toolbar

--- README.md
+++ README.md
@@ -7,10 +7,6 @@ firebase backend and deployment/hosting
 
 custom-search-api
 
-programmable search
+programmable search engine from google
 
-<script async src="https://cse.google.com/cse.js?cx=a7c1ddbc0f9474f46">
-</script>
-<div class="gcse-search"></div>
 
-2.19.49.00
--- google-clone/src/pages/SearchPage.js
+++ google-clone/src/pages/SearchPage.js
@@ -2,17 +2,31 @@ import React from 'react';
 import { useStateValue } from '../StateProvider';
 import useGoogleSearch from '../useGoogle';
 import './SearchPage.css';
+import response from '../Response';
+import { Link } from '@mui/material';
+import Search from '../components/SearchBar';
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
-  const { data } = useGoogleSearch(term);
+  // const { data } = useGoogleSearch(term);
 
+  
+  const data = response;
   console.log(data);
 
   return (
     <div className='search-page'>
       <div className='search-page-header'>
-        <h1> { term } </h1>
+        <Link to="/">
+          <img className='search-page-logo' src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png' alt='google'/>
+          
+
+        </Link>
+        <div className='search-page-header-body'>
+          <Search hideButtons />
+
+        </div>
+        
 
       </div>
       <div className='search-page-results'>
